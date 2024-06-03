be6e5c741b110658b973d0159c4a0b0f011bc8e3
jielim36 <jielim00@gmail.com>
2024-06-03 12:37:01 +0800

    Add Response Class for return response body uniformly.

---
+++ src/main/java/com/jielim36/devgram/common/Response/ResultCode.java
@@ -0,0 +1,25 @@
+package com.jielim36.devgram.common.Response;
+
+public enum  ResultCode {
+    SUCCESS(200, "Success"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    METHOD_NOT_ALLOWED(405,"Method Not Allowed");
+
+    private Integer code;
+    private String message;
+    ResultCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getResultCode() {
+        return this.code.toString();
+    }
+
+    public String getResultMsg() {
+        return this.message;
+    }
+}
---
+++ src/main/java/com/jielim36/devgram/common/Response/ResultResponse.java
@@ -0,0 +1,56 @@
+package com.jielim36.devgram.common.Response;
+
+import java.io.Serializable;
+
+public class ResultResponse<T> implements Serializable {
+
+    private String code;
+    private String message;
+    private T data;
+    public ResultResponse(ResultCode errorInfo) {
+        this.code = errorInfo.getResultCode();
+        this.message = errorInfo.getResultMsg();
+    }
+    public  static ResultResponse success(){
+        ResultResponse resultResponse = new ResultResponse(ResultCode.SUCCESS);
+        return resultResponse;
+    }
+    public  static<T> ResultResponse success(T t){
+        ResultResponse resultResponse = success();
+        resultResponse.setData(t);
+        return resultResponse;
+    }
+    public  static ResultResponse failure(ResultCode resultCode){
+        ResultResponse resultResponse = new ResultResponse(resultCode);
+        return resultResponse;
+    }
+    public  static<T> ResultResponse failure(ResultCode resultCode, T t){
+        ResultResponse resultResponse = failure(resultCode);
+        resultResponse.setData(t);
+        return resultResponse;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
