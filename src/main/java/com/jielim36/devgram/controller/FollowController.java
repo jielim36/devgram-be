8135365afd80a5dfc21a99076e5803e2fa54cda3
jielim36 <jielim00@gmail.com>
2024-06-17 15:45:00 +0800

    Updated follow controller for completed response format

--- src/main/java/com/jielim36/devgram/controller/FollowController.java
+++ src/main/java/com/jielim36/devgram/controller/FollowController.java
@@ -1,6 +1,8 @@
 package com.jielim36.devgram.controller;
 
 import com.jielim36.devgram.DTO.UserDTO;
+import com.jielim36.devgram.common.ResultResponse;
+import com.jielim36.devgram.enums.ResultCode;
 import com.jielim36.devgram.service.FollowService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,38 +18,58 @@ public class FollowController {
     }
 
     @PostMapping
-    public boolean addFollow(Long follower_id, Long following_id) {
-        return followService.addFollow(follower_id, following_id);
+    public ResultResponse addFollow(Long follower_id, Long following_id) {
+        boolean isSuccess = followService.addFollow(follower_id, following_id);
+
+        if(isSuccess) {
+            return ResultResponse.success(true);
+        } else {
+            return ResultResponse.failure(ResultCode.INTERNAL_SERVER_ERROR, "Failed to follow.");
+        }
     }
 
     @PutMapping
-    public boolean unfollow(Long follower_id, Long following_id) {
-        return followService.unfollow(follower_id, following_id);
+    public ResultResponse unfollow(Long follower_id, Long following_id) {
+        boolean unfollow = followService.unfollow(follower_id, following_id);
+
+        if(unfollow) {
+            return ResultResponse.success(true);
+        } else {
+            return ResultResponse.failure(ResultCode.INTERNAL_SERVER_ERROR, "Failed to unfollow.");
+        }
     }
 
     @GetMapping("/isFollowing")
-    public boolean isFollowing(Long follower_id, Long following_id) {
-        return followService.isFollowing(follower_id, following_id);
+    public ResultResponse isFollowing(Long follower_id, Long following_id) {
+        boolean isFollowing = followService.isFollowing(follower_id, following_id);
+
+        return ResultResponse.success(isFollowing);
     }
 
     @GetMapping("/following")
-    public UserDTO[] getFollowingByUserId(Long follower_id, Integer pages) {
-        return followService.getFollowingByUserId(follower_id, pages);
+    public ResultResponse getFollowingByUserId(Long follower_id, Integer pages) {
+        UserDTO[] followingByUserId = followService.getFollowingByUserId(follower_id, pages);
+
+        return ResultResponse.success(followingByUserId);
     }
 
     @GetMapping("/follower")
-    public UserDTO[] getFollowerByUserId(Long following_id, Integer pages) {
-        return followService.getFollowerByUserId(following_id, pages);
+    public ResultResponse getFollowerByUserId(Long following_id, Integer pages) {
+        UserDTO[] followerByUserId = followService.getFollowerByUserId(following_id, pages);
+
+        return ResultResponse.success(followerByUserId);
     }
 
     @GetMapping("/following/count")
-    public int getFollowingCount(Long follower_id) {
-        return followService.getFollowingCount(follower_id);
+    public ResultResponse getFollowingCount(Long follower_id) {
+        int followingCount = followService.getFollowingCount(follower_id);
+        return ResultResponse.success(followingCount);
     }
 
     @GetMapping("/follower/count")
-    public int getFollowerCount(Long following_id) {
-        return followService.getFollowerCount(following_id);
+    public ResultResponse getFollowerCount(Long following_id) {
+        int followerCount = followService.getFollowerCount(following_id);
+        return ResultResponse.success(followerCount);
     }
 
 }
